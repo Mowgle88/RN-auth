09f5bd9adc9e1966407878b3920edc4e32c0d239
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2022-09-05 02:13:16 +0300

    feat: add accessing protected resources

--- screens/WelcomeScreen.tsx
+++ screens/WelcomeScreen.tsx
@@ -1,10 +1,24 @@
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AuthContext } from '../store/auth-context';
+import { fetchMessage } from '../util/http';
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState('');
+
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  useEffect(() => {
+    fetchMessage(token).then(response => setFetchedMessage(response))
+  }, [token])
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
