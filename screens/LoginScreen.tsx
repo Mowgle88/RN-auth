02f35541af8638d33384cef9e7b3633a3810cff1
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2022-09-04 23:51:37 +0300

    feat: add authentication error handling

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Alert } from 'react-native';
 import AuthContent from '../components/Auth/AuthContent';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
 import { login } from '../util/auth';
@@ -8,7 +9,14 @@ function LoginScreen() {
 
   async function signupHandler({ email, password }: { email: string, password: string }) {
     setIsAuthenticating(true);
-    await login(email, password);
+    try {
+      await login(email, password);
+    } catch (error) {
+      Alert.alert(
+        'Authentication failed!',
+        'Could not log you in. Please check your credentials or try again later!'
+      )
+    }
     setIsAuthenticating(false);
   }
 
