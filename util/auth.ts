f8a1f5e28c1efdf4bf0c6d5c34e8c7b1d9b1ef61
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2022-09-05 00:29:28 +0300

    feat: extract authentication token

--- util/auth.ts
+++ util/auth.ts
@@ -1,13 +1,22 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 type mode = 'signInWithPassword' | 'signUp';
+
+interface IAuthResponseData {
+  idToken: string,
+  email: string,
+  refreshToken: string,
+  expiresIn: string,
+  localId: string,
+  registered: boolean
+}
 
 const API_KEY = 'AIzaSyDKlptjjDNYj9d3LaPONPxQUjpYxiaUF34';
 
 export async function authenticate(mode: mode, email: string, password: string) {
   const URL = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
 
-  const response = await axios.post(
+  const response: AxiosResponse<IAuthResponseData> = await axios.post(
     URL,
     {
       email: email,
@@ -15,13 +24,15 @@ export async function authenticate(mode: mode, email: string, password: string) 
       returnSecureToken: true
     }
   )
-  console.log(response.data);
+
+  const token = response.data.idToken;
+  return token;
 }
 
-export async function createUser(email: string, password: string) {
-  await authenticate('signUp', email, password);
+export function createUser(email: string, password: string) {
+  return authenticate('signUp', email, password);
 }
 
-export async function login(email: string, password: string) {
-  await authenticate('signInWithPassword', email, password);
+export function login(email: string, password: string) {
+  return authenticate('signInWithPassword', email, password);
 }
